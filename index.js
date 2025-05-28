50260a9d3df0f96e894e6b07367ff26f8447fe57
Arnauw <arnaud.rabel@gmail.com>
2025-05-28 16:25:19 +0200

    test tailwind

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+console.log('index.js loaded successfully.')
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.body.style.backgroundColor = 'black';
+// });
+
+document.body.style.backgroundColor = 'black';
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//   // ...
+//   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+// };
+
+// tailwind.config.js
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+// }
